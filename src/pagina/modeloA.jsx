cec6f3dcef88d7952faba5060b8b36e95229a0f3
Brian AB <brianangulo900@gmail.com>
2025-12-10 00:51:02 +0000

    ultimo3

--- src/pagina/modeloA.jsx
+++ src/pagina/modeloA.jsx
@@ -45,7 +45,7 @@ export default function MA() {
                       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
                       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
                     </svg>
-                    <span><strong>Mercería Calderon</strong> - Zona 12 de Octubre Calle Raul Salmon entre calle 4 y calle 3</span>
+                    <span><strong>Mercería Amelia</strong> - Zona 12 de Octubre Calle Raul Salmon entre calle 4 y calle 3</span>
                   </p>
                 </div>
               </div>
